09377af178ff2ad568856643e26ca614e65e793c
qasemB <mahdicmptr@gmail.com>
2021-07-06 00:55:08 +0430

    part #29

--- app/Http/Livewire/Search/Search.php
+++ app/Http/Livewire/Search/Search.php
@@ -2,13 +2,54 @@
 
 namespace App\Http\Livewire\Search;
 
+use App\Models\Article;
+use App\Models\Category;
 use Livewire\Component;
 
 class Search extends Component
 {
 
+    public $articles;
+    public $categories;
+    public $categoryId;
+    public $char="";
+
+    public function updatedCategoryId(){
+        return redirect()->to('/search/'.$this->categoryId.'/'.$this->char);
+    }
+
+    public function updatedChar(){
+        return redirect()->to('/search/'.$this->categoryId.'/'.$this->char);
+    }
+
     public function mount($catId , $char = ""){
-        dd([$catId , $char]);
+
+        $this->categories = Category::all();
+        $this->categoryId = $catId;
+        $this->char = $char;
+
+        if($catId == 0) {
+
+            $result = Article::where('h_title' , 'like' , '%'.$char.'%')
+            ->orWhere('top_title' , 'like' , '%'.$char.'%')
+            ->orWhere('text' , 'like' , '%'.$char.'%')
+            ->get();
+            $this->articles = $result;
+
+        } else{
+
+            $result = Article::where([
+                ['category_id' , $catId],
+                ['h_title' , 'like' , '%'.$char.'%'],
+            ])->orWhere([
+                ['category_id' , $catId],
+                ['top_title' , 'like' , '%'.$char.'%'],
+            ])->orWhere([
+                ['category_id' , $catId],
+                ['text' , 'like' , '%'.$char.'%'],
+            ])->get();
+            $this->articles = $result;
+        }
     }
 
     public function render()
--- resources/views/livewire/search/search.blade.php
+++ resources/views/livewire/search/search.blade.php
@@ -7,15 +7,16 @@
             type="text"
             class="form-control rounded_5 placeholder_gray shadow-sm h_2_5"
             placeholder="عنوان رو وارد کن"
+            wire:model.debounce.1000ms="char"
           />
           <a href="/search.html" class="fas fa-search search_btn"></a>
         </div>
         <div class="col-12 col-md-6 form-group search_box">
-          <select name="" id="" class="rounded_5 outline_0 h_2_5 border-0 form-control">
-            <option value="">انتخاب بر اساس گروه</option>
-            <option value="">انتخاب سوم</option>
-            <option value="">گروه اول</option>
-            <option value="">گروه دوم</option>
+          <select name="" id="" class="rounded_5 outline_0 h_2_5 border-0 form-control" wire:model="categoryId">
+            <option value="0">انتخاب بر اساس گروه</option>
+            @foreach ($categories as $cat)
+            <option value="{{$cat->id}}">{{$cat->title}}</option>
+            @endforeach
           </select>
         </div>
       </div>
@@ -26,105 +27,12 @@
       </div>
 
       <div class="row col-12 col-md-11 justify-content-center align-items-center">
-        <div class="col-12 col-md-3 py-4 ">
-          <div class="card hover_shadow hover_up">
-            <a href="/article.html">
-            <div class="p-0 over_hidden card-header h_10 d-flex align-items-center justify-content-center">
-              <img src="./assets/images/articles/article1.jpg" alt="" class="h-100">
-            </div>
-            <div class="card-body px-1 py-2">
-              <h5 class="text-center text-primary">عنوان مقاله 1</h5>
-              <p class="text-justify text-right font_0_9 text-secondary">
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-              </p>
-              <a href="#" class="btn btn-primary cursor_pointer_shadow rounded_5 px-3">ادامه مقاله</a>
-            </div>
-          </a>
-          </div>
-        </div>
 
-        <div class="col-12 col-md-3 px-3 py-4 ">
-          <div class="card hover_shadow hover_up">
-            <a href="/article.html">
-            <div class="p-0 over_hidden card-header h_10 d-flex align-items-center justify-content-center">
-              <img src="./assets/images/articles/article1.jpg" alt="" class="h-100">
-            </div>
-            <div class="card-body px-1 py-2">
-              <h5 class="text-center text-primary">عنوان مقاله 1</h5>
-              <p class="text-justify text-right font_0_9 text-secondary">
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-              </p>
-              <a href="#" class="btn btn-primary cursor_pointer_shadow rounded_5 px-3">ادامه مقاله</a>
-            </div>
-          </a>
-          </div>
-        </div>
 
-        <div class="col-12 col-md-3 px-3 py-4 ">
-          <div class="card hover_shadow hover_up">
-            <a href="/article.html">
-            <div class="p-0 over_hidden card-header h_10 d-flex align-items-center justify-content-center">
-              <img src="./assets/images/articles/article1.jpg" alt="" class="h-100">
-            </div>
-            <div class="card-body px-1 py-2">
-              <h5 class="text-center text-primary">عنوان مقاله 1</h5>
-              <p class="text-justify text-right font_0_9 text-secondary">
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-              </p>
-              <a href="#" class="btn btn-primary cursor_pointer_shadow rounded_5 px-3">ادامه مقاله</a>
-            </div>
-          </a>
-          </div>
-        </div>
+        @foreach ($articles as $article)
+        <livewire:index.article-card :article="$article">
+        @endforeach
 
-        <div class="col-12 col-md-3 px-3 py-4 ">
-          <div class="card hover_shadow hover_up">
-            <a href="/article.html">
-            <div class="p-0 over_hidden card-header h_10 d-flex align-items-center justify-content-center">
-              <img src="./assets/images/articles/article1.jpg" alt="" class="h-100">
-            </div>
-            <div class="card-body px-1 py-2">
-              <h5 class="text-center text-primary">عنوان مقاله 1</h5>
-              <p class="text-justify text-right font_0_9 text-secondary">
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-              </p>
-              <a href="#" class="btn btn-primary cursor_pointer_shadow rounded_5 px-3">ادامه مقاله</a>
-            </div>
-          </a>
-          </div>
-        </div>
-
-        <div class="col-12 col-md-3 px-3 py-4 ">
-          <div class="card hover_shadow hover_up">
-            <a href="/article.html">
-            <div class="p-0 over_hidden card-header h_10 d-flex align-items-center justify-content-center">
-              <img src="./assets/images/articles/article1.jpg" alt="" class="h-100">
-            </div>
-            <div class="card-body px-1 py-2">
-              <h5 class="text-center text-primary">عنوان مقاله 1</h5>
-              <p class="text-justify text-right font_0_9 text-secondary">
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-                توضیح کوتاه در مورد این مقاله
-              </p>
-              <a href="#" class="btn btn-primary cursor_pointer_shadow rounded_5 px-3">ادامه مقاله</a>
-            </div>
-          </a>
-          </div>
-        </div>
       </div>
 
 
